3d003bb0037175a6c49097c2edc216c7c63e556e
Patrício Samuel <patricioanjosjf@gmail.com>
2023-03-15 20:37:42 -0300

    versão 2.0

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// const inputUsuario = document.getElementById('newTask');
 const ul = document.getElementById('ul');
 
 const modal = document.querySelector('dialog')
@@ -13,34 +12,28 @@ buttonCloseModal.onclick = () => modal.close()
 buttonAddNewTask.onclick = adicionarTarefa
 
 function adicionarTarefa() {
-    if(true) {
-        const taskTitle = document.getElementById('taskTitle')
-        const taskName = document.getElementById('taskName')
-    
-        const div = document.createElement('div')
-        const h2 = document.createElement('h2')
-        const li = document.createElement('li');
-    
-        h2.innerText = taskTitle.value
-        li.innerText = taskName.value
-        div.append(h2, li)
-        ul.appendChild(div)
+    const taskName = document.getElementById('taskName')
 
-        taskName.value = '';
-    }else {
-        
-    }
-    
-    
-    //criando botão para excluir tarefa
+    const div = document.createElement('div')
+    const h2 = document.createElement('h2')
+    const p = document.createElement('p');
     const botaoExcluirTarefa = document.createElement('button');
-    botaoExcluirTarefa.appendChild(document.createTextNode('X'));
-    li.appendChild(botaoExcluirTarefa);
-    botaoExcluirTarefa.addEventListener('click', deletarTarefa);
     
+    botaoExcluirTarefa.innerText = 'X';
+    p.innerText = taskName.value
+    div.append(h2, p, botaoExcluirTarefa)
+    ul.appendChild(div)
+
+    taskName.value = '';
+
+    //criando botão para excluir tarefa
+    botaoExcluirTarefa.addEventListener('click', deletarTarefa);
+
     function deletarTarefa() {
-        li.remove();
+        div.remove();
     }
+
+    modal.close()
 }
 
 // inputUsuario.addEventListener('keypress', function (event){
